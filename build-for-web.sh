10888f58c707d3684ab88ec6415ef940a4d71b8c
Alex <git@pentive.com>
2023-10-13 16:24:31 -0500

    fix build

--- build-for-web.sh
+++ build-for-web.sh
@@ -15,5 +15,5 @@ export RUSTFLAGS="-C lto=fat -C embed-bitcode=yes -C codegen-units=1 -C opt-leve
 
 # Run wasm pack tool to build JS wrapper files and copy wasm to pkg directory.
 mkdir -p pkg
-wasm-pack build --out-dir pkg --release --target web --no-typescript --no-default-features --features $1
+wasm-pack build --out-dir pkg --release --target web --no-typescript
 
