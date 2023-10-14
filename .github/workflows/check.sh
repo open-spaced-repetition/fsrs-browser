79d66ebeaca5ca0dc8a7f1a850230a9e3e9ee61f
Jarrett Ye <jarrett.ye@outlook.com>
2023-10-14 17:51:39 +0800

    Add CI

---
+++ .github/workflows/check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cargo fmt --check || (
+    echo
+    echo "Please run 'cargo fmt' to format the code."
+    exit 1
+)
+
+cargo clippy -- -Dwarnings
+
+cargo test --release
