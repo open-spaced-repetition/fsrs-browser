2b97143777a7bd16c35f3a41c125a31c26a8335a
Alex <git@pentive.com>
2023-10-26 17:09:01 -0500

    npx prettier --ignore-path .gitignore --write .

--- sandbox/src/index.tsx
+++ sandbox/src/index.tsx
@@ -1,22 +1,22 @@
 /* @refresh reload */
-import 'bootstrap/scss/bootstrap.scss';
+import 'bootstrap/scss/bootstrap.scss'
 
-import { render } from 'solid-js/web';
+import { render } from 'solid-js/web'
 
 /**
  * This file was taken from the cheatsheet example of bootstrap.
  * You will most likely remove it if using this template.
  */
-import './cheatsheet.scss';
+import './cheatsheet.scss'
 
-import App from './App';
+import App from './App'
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
+	throw new Error(
+		'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+	)
 }
 
-render(() => <App />, root!);
+render(() => <App />, root!)
--- sandbox/vite.config.ts
+++ sandbox/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
+	plugins: [
+		/* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+		// devtools(),
+		solidPlugin(),
+	],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
+})
