e084cee5f00794dad8269c62f9138ae0eaeb2e71
Alex <git@pentive.com>
2024-02-06 16:14:10 -0600

    update build

--- build.sh
+++ build.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -eo pipefail # https://stackoverflow.com/a/2871034
 
 # Add wasm32 target for compiler.
 rustup target add wasm32-unknown-unknown
 
-if ! command -v wasm-pack &> /dev/null
-then
-    echo "wasm-pack could not be found. Installing ..."
-    cargo install wasm-pack
-    exit
+if ! command -v wasm-pack &>/dev/null; then
+	echo "wasm-pack could not be found. Installing ..."
+	cargo install wasm-pack
+	exit
 fi
 
 # Set optimization flags
-export RUSTFLAGS="-C lto=fat -C embed-bitcode=yes -C codegen-units=1 -C opt-level=3 --cfg web_sys_unstable_apis"
+if [[ $1 == "release" ]]; then
+	export RUSTFLAGS="-C lto=fat -C embed-bitcode=yes -C codegen-units=1 -C opt-level=3 --cfg web_sys_unstable_apis"
+else
+	# sets $1 to "dev"
+	set -- dev
+fi
 
 # Run wasm pack tool to build JS wrapper files and copy wasm to pkg directory.
 mkdir -p pkg
-wasm-pack --verbose build --out-dir pkg ${BUILD_TYPE} --target web
-
+wasm-pack build --out-dir pkg --$1 --target web --no-default-features
