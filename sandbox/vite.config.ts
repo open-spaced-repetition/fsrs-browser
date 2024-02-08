2ce6c91369f48d27d4ce28380704ce07f450c2af
Alex <git@pentive.com>
2024-02-07 20:56:25 -0600

    add coop/coep

--- sandbox/vite.config.ts
+++ sandbox/vite.config.ts
@@ -10,6 +10,17 @@ export default defineConfig({
     */
 		// devtools(),
 		solidPlugin(),
+		{
+			// https://gist.github.com/mizchi/afcc5cf233c9e6943720fde4b4579a2b
+			name: 'isolation',
+			configureServer(server) {
+				server.middlewares.use((_req, res, next) => {
+					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+					res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+					next()
+				})
+			},
+		},
 	],
 	server: {
 		port: 3000,
