b2f1a6aa00248530442e3c8076ac2356a58a0c4a
Alex <git@pentive.com>
2024-02-28 11:50:39 -0600

    if prod use 'rollup-plugin-off-main-thread'

--- sandbox/vite.config.ts
+++ sandbox/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { UserConfig, defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import offMainThread from '@surma/rollup-plugin-off-main-thread'
 // import devtools from 'solid-devtools/vite';
 
-export default defineConfig({
-	plugins: [
+export default defineConfig(({ mode }: UserConfig) => {
+	let plugins = [
 		/* 
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
@@ -21,15 +22,21 @@ export default defineConfig({
 				})
 			},
 		},
-	],
-	server: {
-		port: 3000,
-		fs: {
-			// Allow serving files from one level up to the project root
-			allow: ['..'],
+	]
+	if (mode === 'production') {
+		plugins.push(offMainThread())
+	}
+	return {
+		plugins,
+		server: {
+			port: 3000,
+			fs: {
+				// Allow serving files from one level up to the project root
+				allow: ['..'],
+			},
 		},
-	},
-	build: {
-		target: 'esnext',
-	},
+		build: {
+			target: 'esnext',
+		},
+	}
 })
