06d344cb1d89153cb9b3837a56c8807418c89a67
Alex <git@pentive.com>
2024-03-27 11:53:44 -0500

    oops

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ rustup target add wasm32-unknown-unknown
 if ! command -v wasm-pack &>/dev/null; then
 	echo "wasm-pack could not be found. Installing ..."
 	cargo install wasm-pack
-	exit
 fi
 
 # Set optimization flags
